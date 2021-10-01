03a94269c57e8a61e94034e4ba288f9bd43b8a19
greg mathias <mathiasgreg@gmail>
2021-10-01 12:58:54 -0400

    first

--- my_scripts/hello.R
+++ my_scripts/hello.R
@@ -1,13 +1,6 @@
-## --- itngscript.R
-# A script to analyse nightgarden.Rdata
-# author: Greg Mathias
-# date: 2021/09/30
+## --- functionexample2.R
 
-# Load data, tell user what we're doing
-cat( "loading data from nightgarden.Rdata...\n" )
-load(file.path("data/nightgarden.Rdata"))
-
-# Create cross tabulation and print it out:
-cat( "tabulating data...\n" )
-itng.table <- table( speaker, utterance )
-print( itng.table )
+pow <- function( x, y=1 ) {
+  out <- x^y # raise x to power y
+  return( out )
+}
